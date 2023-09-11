697a1b12e7d23b0204f67f7162c04772d163d0e7
jdelg140 <jacobdelgado140@gmail.com>
2023-09-11 19:16:46 -0400

    resolved merge conflicts

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 const Events = () => {
@@ -9,23 +8,10 @@ const Events = () => {
           <div className='flex justify-center bg-gray-500 rounded-md items-center'>
             <div className=' justify-center text-center p-4'>
               <p className='text-2xl tracking-wider'>
-=======
-import React from "react";
-
-const Events = () => {
-  return (
-    <div className="bg-[#2f3235] text-gray-300 w-full md:h-screen">
-      <div className="grid grid-cols-6 gap-4">
-        <div className="col-start-2 col-span-4 p-4">
-          <div className="flex justify-center bg-gray-500 rounded-md items-center">
-            <div className=" justify-center text-center p-4">
-              <p className=" object-top text-2xl font-bold text-white tracking-wider">
->>>>>>> main
                 List of Events
               </p>
             </div>
           </div>
-<<<<<<< HEAD
           <div className='flex flex-row justify-end my-4 space-x-2'>
             <div className='bg-gray-500 rounded-md p-4 gap-4'>
               <button className=' text-lg tracking-wider'>
@@ -49,31 +35,6 @@ const Events = () => {
                 </li>
               </ul>
               <button className='col-start-4 col-end-4'>Add To Cart</button>
-=======
-          <div className="flex flex-row justify-end my-4 space-x-2">
-            <div className="bg-gray-500 rounded-md p-4 gap-4">
-              <span className="object-top text-lg font-bold text-white tracking-wider">
-                Filter
-              </span>
-            </div>
-            <div className="bg-gray-500 rounded-md p-4">
-              <span className=" text-lg font-bold text-white tracking-wider">
-                Sort
-              </span>
-            </div>
-          </div>
-          <div className=" bg-gray-500 rounded-md my-4">
-            <div className=" grid grid-cols-4 p-4">
-              <ul>
-                <li className="col-start-1 col-end-1 font-bold text-white tracking-wider">
-                  <p>TITLE:</p>
-                  <p>DESCRIPTION:</p>
-                  <p>DATE:</p>
-                  <p>PRICE:</p>
-                </li>
-              </ul>
-              <button className="col-start-4 col-end-4">Add To Cart</button>
->>>>>>> main
             </div>
           </div>
         </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 const Home = () => {
@@ -17,36 +16,12 @@ const Home = () => {
                 Create Your Event
               </p>
               <div className='pt-8 '>
-=======
-import React from "react";
-
-const Home = () => {
-  return (
-    <div
-      name="home"
-      className="bg-[#2f3235] text-gray-300 w-full h-full lg:min-h-screen bottom-0"
-    >
-      <div className="w-full p-4 flex flex-col justify-center h-screen bg-blue-500 ">
-        <div>
-        <h1 className="text-4xl font-bold text-white mb-4 sm:hidden">Welcome to Event Share!</h1>
-        </div>
-        
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 grid-shr ">
-          <div className=" bg-gray-500 group container rounded-md flex justify-center items-center mx-auto">
-            
-            <div className="p-4">
-              <p className="text-2xl font-bold text-white tracking-wider">
-                Create Your Event
-              </p>
-              <div className="pt-8 ">
->>>>>>> main
                 <p>
                   Are you an event organizer, artist, or performer? Showcase
                   your talents and create your own event listings effortlessly.
                   Our user-friendly platform allows you to craft captivating
                   event pages, set ticket prices, and reach a broad audience.
                 </p>
-<<<<<<< HEAD
               </div>
             </div>
           </div>
@@ -56,18 +31,6 @@ const Home = () => {
                 Discover Amazing Events
               </p>
               <div className='pt-8 '>
-=======
-                <p></p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-gray-500 group container rounded-md flex justify-center items-center mx-auto">
-            <div className="p-4">
-              <p className="text-2xl font-bold text-white tracking-wider">
-                Discover Amazing Events
-              </p>
-              <div className="pt-8 ">
->>>>>>> main
                 <p>
                   Explore a diverse range of events happening near you or in
                   your favorite cities. From music concerts and art exhibitions
@@ -75,7 +38,6 @@ const Home = () => {
                   intuitive search and filter options make finding the perfect
                   event a breeze.
                 </p>
-<<<<<<< HEAD
               </div>
             </div>
           </div>
@@ -85,25 +47,12 @@ const Home = () => {
                 Buy Tickets with Ease
               </p>
               <div className='pt-8 '>
-=======
-                <p></p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-gray-500 group container rounded-md flex justify-center items-center mx-auto">
-            <div className="p-4">
-              <p className="text-2xl font-bold text-white tracking-wider">
-                Buy Tickets with Ease
-              </p>
-              <div className="pt-8 ">
->>>>>>> main
                 <p>
                   Secure your spot at your preferred events hassle-free.
                   Purchase single or multiple tickets for you and your friends
                   with a few simple clicks. Our secure payment system ensures
                   your transactions are safe and smooth.
                 </p>
-<<<<<<< HEAD
               </div>
             </div>
           </div>
@@ -114,40 +63,12 @@ const Home = () => {
                 Connect with the Community
               </p>
               <div className='pt-8 '>
-=======
-                <p></p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-gray-500 group container rounded-md flex justify-center items-center mx-auto">
-            <div className="p-4">
-              <p className="text-2xl font-bold text-white tracking-wider">
-                Stay Informed
-              </p>
-              <div className="pt-8 ">
-                <p>
-                  Stay updated with event details, schedules, and important
-                  announcements. Receive email notifications, reminders, and
-                  personalized recommendations based on your interests.
-                </p>
-                <p></p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-gray-500 group container rounded-md flex justify-center items-center mx-auto">
-            <div className="p-4">
-              <p className="text-2xl font-bold text-white tracking-wider">
-                Connect with the Community
-              </p>
-              <div className="pt-8 ">
->>>>>>> main
                 <p>
                   Join a vibrant community of event enthusiasts. Share your
                   experiences, reviews, and photos with fellow attendees.
                   Connect with event organizers and artists to build lasting
                   connections.
                 </p>
-<<<<<<< HEAD
               </div>
             </div>
           </div>
@@ -163,28 +84,11 @@ const Home = () => {
                   aficionado, a sports fanatic, or a foodie, there's something
                   here for you.
                 </p>
-=======
-                <p></p>
-              </div>
-            </div>
-          </div>
-          <div className=" bg-gray-500 group container rounded-md flex justify-center items-center mx-auto">
-            <div className="p-4">
-              <p className="text-2xl font-bold text-white tracking-wider">
-              Why Choose Us?
-              </p>
-              <div className="pt-8 ">
-                <p>
-                Unmatched Variety: We curate a diverse selection of events to cater to all tastes and preferences. Whether you're a music aficionado, a sports fanatic, or a foodie, there's something here for you.
-                </p>
-                <p></p>
->>>>>>> main
               </div>
             </div>
           </div>
         </div>
       </div>
-<<<<<<< HEAD
 
       {/* slider */}
       <div className='carousel w-full h-screen md:hidden pt-32 '>
@@ -292,8 +196,6 @@ const Home = () => {
           5
         </a>
       </div>
-=======
->>>>>>> main
     </div>
   );
 };
