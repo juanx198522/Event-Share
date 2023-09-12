4a9ec7b10767cdcbed66c6c3dfbef8b8587186ad
May Peralta <129072711+mayperalta@users.noreply.github.com>
2023-09-11 23:00:55 -0400

    Added App.js

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Events from './pages/Events'
+import MyEvents from './pages/MyEvents'
+import Booking from './pages/Booking'
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/events' element={<Events />} />
+            <Route path='/myevents' element={<MyEvents />} />
+            <Route path='/booking' element={<Booking />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Router>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
