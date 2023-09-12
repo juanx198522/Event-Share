61fba80f96d33ff8f5ce25715c50312f2e87dffa
juanx198522 <juanx1985@icloud.com>
2023-09-11 22:36:01 -0400

    Create and event in MyEvents set

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -9,21 +8,27 @@ import Events from './pages/Events'
 import MyEvents from './pages/MyEvents'
 import Booking from './pages/Booking'
 
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/events' element={<Events />} />
-          <Route path='/myevents' element={<MyEvents />} />
-          <Route path='/booking' element={<Booking />} />
-          <Route path='/login' element={<Login />} />
-        </Routes>
-      </Router>
-
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/events' element={<Events />} />
+            <Route path='/myevents' element={<MyEvents />} />
+            <Route path='/booking' element={<Booking />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 
