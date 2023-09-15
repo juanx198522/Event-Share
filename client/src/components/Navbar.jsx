1304e7be12ce76340be1411c3c292694953c00b1
jdelg140 <jacobdelgado140@gmail.com>
2023-09-15 18:33:25 -0400

    made logout appear on navbar when user is logged in

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Logo from '../assets/eventlogo.png'
-import Auth from '../utils/authClient';
+import Logo from "../assets/eventlogo.png";
+import Auth from "../utils/authClient";
 
 const Navbar = () => {
   const logout = (event) => {
@@ -10,20 +10,24 @@ const Navbar = () => {
     Auth.logout();
   };
   return (
-    <div className='fixed w-screen h-[12%] flex justify-between items-center p-8 bg-[#001e3b] text-white'>
+    <div className="fixed w-screen h-[12%] flex justify-between items-center p-8 bg-[#001e3b] text-white">
       <div>
         <ul>
           <li>
-            <Link to='' duration={500}>
-              <div className=''>
-              <img className='w-50 h-20 items-center object-cover h-10 w-60' src={Logo} alt='EventLogo'/>
+            <Link to="" duration={500}>
+              <div className="">
+                <img
+                  className="w-50 h-20 items-center object-cover h-10 w-60"
+                  src={Logo}
+                  alt="EventLogo"
+                />
               </div>
             </Link>
           </li>
         </ul>
-        </div>
-        <div>
-        <ul className=' flex justify-items-end'>
+      </div>
+      <div>
+        <ul className=" flex justify-items-end">
           <li>
             <Link to="/events" duration={500}>
               Events
@@ -31,7 +35,6 @@ const Navbar = () => {
           </li>
           <li>
             <Link to="/myevent" duration={500}>
-              
               My Events
             </Link>
           </li>
@@ -41,34 +44,35 @@ const Navbar = () => {
             </Link>
           </li>
           <li>
-          {Auth.loggedIn() ? (
-            <>
-              <div className="btn btn-lg btn-info m-2">
-                {Auth.getProfile().data.username}'s profile
-              </div>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/register">
-                Signup
-              </Link>
-            </>
-          )}
-            <Link to="/login" duration={500}>
+            {Auth.loggedIn() ? (
+              <>
+                <div className="">
+                  {Auth.getProfile().data.username}'s profile
+                </div>
+                <button className="btn btn-lg btn-light m-2 hover:text-red-300" onClick={logout}>
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link className="btn btn-lg btn-info m-2 " to="/login">
+                  Login
+                </Link>
+                <Link className="btn btn-lg btn-light m-2" to="/register">
+                  Signup
+                </Link>
+              </>
+            )}
+
+          
+            {/* <Link to="/login" duration={500}>
               Signup/Login
-            </Link>
+            </Link> */}
           </li>
         </ul>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
